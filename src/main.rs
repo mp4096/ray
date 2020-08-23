756d84afe51673c43e87a5bdb917ab58214d1f62
Mikhail Pak <code@mp4096.com>
2020-08-23 11:04:13 +0200

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::prelude::*;
 use std::io::BufWriter;
 
 use itertools::iproduct;
+use indicatif::ProgressBar;
 
 struct Pixel {
     red: u8,
@@ -24,9 +25,15 @@ fn write_ppm(width: usize, height: usize, pixels: &[Pixel]) -> std::io::Result<(
 fn main() {
     let width = 1920;
     let height = 1080;
-    println!("Writing a {}x{} image", width, height);
+    let total_pixels = width * height;
+
     let mut vec: Vec<Pixel> = Vec::with_capacity(width * height);
-    for (j, i) in iproduct!((0..height).rev(), 0..width) {
+    let coordinates_range = iproduct!((0..height).rev(), 0..width);
+    let pb = ProgressBar::new(total_pixels as u64);
+    pb.set_draw_delta((total_pixels / 100) as u64);
+
+    println!("Writing a {}x{} image", width, height);
+    for (j, i) in pb.wrap_iter(coordinates_range) {
         let r = (i as f64) / ((width - 1) as f64);
         let g = (j as f64) / ((height - 1) as f64);
         let b = 0.25;
