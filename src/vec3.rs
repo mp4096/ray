2b9b9193ed0bfc88c9f3a0a6dd0d134282104346
Sebastian <sew@posteo.de>
2020-08-23 13:09:34 +0300

    seb's update

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,4 @@
-use std::ops::Neg;
+use std::ops::{AddAssign, DivAssign, Index, MulAssign, Neg};
 
 pub struct Vec3 {
     pub x: f64,
@@ -18,6 +18,14 @@ impl Vec3 {
     pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
         Vec3 { x, y, z }
     }
+
+    pub fn squared_length(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn length(&self) -> f64 {
+        self.squared_length().sqrt()
+    }
 }
 
 impl Neg for Vec3 {
@@ -29,5 +37,63 @@ impl Neg for Vec3 {
             y: -self.y,
             z: -self.z,
         }
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        };
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        *self = Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        };
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        *self *= 1.0_f64 / rhs;
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => &f64::NAN,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Vec3;
+
+    #[test]
+    fn it_works() {
+        let vector = Vec3::new(1_f64, 1_f64, 1_f64);
+        println!("length {}", vector.length());
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn this_too() {
+        let vector = Vec3::new(1_f64, 2_f64, 3_f64);
+        println!("length {}, {}, {}", vector[0], vector[1], vector[2]);
+        assert_eq!(2 + 2, 4);
     }
 }
